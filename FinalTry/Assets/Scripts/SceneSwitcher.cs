3a1da420e8f737dad06e162e5e2837303c0bcd88
Ceasar Attar <catta2@uic.edu>
2025-03-09 08:23:45 -0500

    added transition into game scene, map loads but character unable to move and car view is messed up

--- FinalTry/Assets/Scripts/SceneSwitcher.cs
+++ FinalTry/Assets/Scripts/SceneSwitcher.cs
@@ -2,27 +2,39 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using Solana.Unity.SDK;
 using Solana.Unity.Wallet;
+using System.Collections;
 
 public class SceneSwitcher : MonoBehaviour
 {
     private void OnEnable()
     {
-        // Subscribe to the OnLogin event when this script is enabled
         Web3.OnLogin += HandleLogin;
+        StartCoroutine(CheckLoginState());
     }
 
     private void OnDisable()
     {
-        // Unsubscribe when the script is disabled to avoid memory leaks
         Web3.OnLogin -= HandleLogin;
+        StopCoroutine(CheckLoginState());
     }
 
     private void HandleLogin(Account account)
     {
-        // This method is called when a successful login occurs
-        Debug.Log($"Successfully logged in with account: {account.PublicKey}");
-        
-        // Switch to the desired scene
+        Debug.Log($"Successfully logged in: {account.PublicKey}");
         SceneManager.LoadScene("Game");
+    }
+
+    private IEnumerator CheckLoginState()
+    {
+        while (true)
+        {
+            if (Web3.Wallet != null && Web3.Wallet.Account != null)
+            {
+                Debug.Log("Detected existing session, switching to Game scene...");
+                SceneManager.LoadScene("Game");
+                yield break;
+            }
+            yield return new WaitForSeconds(1f);
+        }
     }
 }
